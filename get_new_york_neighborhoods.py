2d81375daf840aed48bfc56f49c6a86fde8708e7
Mahim Arib <mahim216@gmail.com>
2018-07-15 16:28:23 -0400

    created get_new_york_neighborhoods.py

---
+++ get_new_york_neighborhoods.py
@@ -0,0 +1,18 @@
+from web_scraping import get_parsed_url
+
+url = 'https://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City'
+page_soup = get_parsed_url(url)
+table_rows = page_soup.table.find_all('tr')
+# data I want to work with, don't need the first row or the last row
+table_data = table_rows[1:-1]
+
+populations = []
+neighborhoods = []
+
+for data in table_data:
+    population = data.find_all('td')[2].get_text()
+    neighborhood = data.find_all('td')[4].get_text()
+    populations.append(population)
+    neighborhoods.append(neighborhood)
+    print("neighborhood: %s" % neighborhood,
+          "\n", "population: %s" % population, "\n")
