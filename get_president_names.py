db6e97aa73c895304b7576140aae5e198466c7fd
Mahim Arib <mahim216@gmail.com>
2018-07-15 20:39:01 -0400

    fixed syntax + comments

--- get_president_names.py
+++ get_president_names.py
@@ -4,8 +4,12 @@ url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States'
 page_soup = get_parsed_url(url)
 tables = page_soup.findAll('table', {'class': 'wikitable'})
 table_rows = tables[0].find_all('tr')
+# the first row does not include the names of presidents
+# created a new array with with the actual working data
 working_data = table_rows[2:]
 
 for row in working_data:
-    if len(row.find_all('td')) >= 4: # because some rows do not contain the presidents name, had to differentiate
+    # because some rows do not contain the presidents name,
+    # had to differentiate the rows
+    if len(row.find_all('td')) >= 4:
         print(row.find_all('td')[3].a.getText())
