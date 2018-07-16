7cf9799e0bbc7e9711526b4e6ba17cfb691d99ab
Mahim Arib <mahim216@gmail.com>
2018-07-16 01:05:27 -0400

    got rid of dependencies

--- movies/center.py
+++ movies/center.py
@@ -1,56 +1,30 @@
 from movie import Movie
 import fresh_tomatoes
 
-toy_story_title = 'Toy Story'
-toy_story_storyline = 'Toys come to life'
-toy_story_poster = 'https://en.wikipedia.org/wiki/File:Toy_Story.jpg'
-toy_story_trailer_url = 'https://www.youtube.com/watch?v=KYz2wyBy3kc'
+toy_story = Movie('Toy Story', 'Toys come to life',
+                  'https://en.wikipedia.org/wiki/File:Toy_Story.jpg',
+                  'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-avatar_title = 'Avatar'
-avatar_story_line = 'A marine on an alien planet'
-avatar_poster = 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg'
-avatar_trailer_url = 'https://www.youtube.com/watch?v=uZNHIU3uHT4'
+avatar = Movie('Avatar', 'A marine on an alien planet',
+               'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg',
+               'https://www.youtube.com/watch?v=uZNHIU3uHT4')
 
-school_of_rock_title = 'School of Rock'
-school_of_rock_storyline = 'Using rock music to learn'
-school_of_rock_poster = 'https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg'
-school_of_rock_trailer_url = 'https://www.youtube.com/watch?v=3PsUJFEBC74'
+school_of_rock = Movie('School of Rock', 'Using rock music to learn',
+                       'https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg',
+                       'https://www.youtube.com/watch?v=3PsUJFEBC74')
 
-ratatouille_title = 'Ratatouille'
-ratatouille_storyline = 'A rat is a chef in Paris'
-ratatouille_poster = 'https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg'
-ratatouille_trailer_url = 'https://www.youtube.com/watch?v=uVeNEbh3A4U'
+ratatouille = Movie('Ratatouille', 'A rat is a chef in Paris',
+                    'https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg',
+                    'https://www.youtube.com/watch?v=uVeNEbh3A4U')
 
-midnight_in_paris_title = 'Midnight in Paris'
-midnight_in_paris_storyline = 'Going back in time to meet the authors'
-midnight_in_paris_poster = 'https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg'
-midnight_in_paris_trailer_url = 'https://www.youtube.com/watch?v=wuOUdZjuCIA'
+midnight_in_paris = Movie('Midnight in Paris',
+                          'Going back in time to meet the authors',
+                          'https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg',
+                          'https://www.youtube.com/watch?v=wuOUdZjuCIA')
 
-hunger_games_title = 'Hunger Games'
-hunger_games_storyline = 'A really real reality show'
-hunger_games_poster = 'https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg'
-hunger_games_trailer_url = 'https://www.youtube.com/watch?v=LrXIG4oK7Ew'
-
-toy_story = Movie(toy_story_title, toy_story_storyline,
-                  toy_story_poster, toy_story_trailer_url)
-
-avatar = Movie(avatar_title, avatar_story_line,
-               avatar_poster, avatar_trailer_url)
-
-school_of_rock = Movie(school_of_rock_title, school_of_rock_storyline,
-                       school_of_rock_poster, school_of_rock_trailer_url)
-
-ratatouille = Movie(ratatouille_title, ratatouille_storyline,
-                    ratatouille_poster, ratatouille_trailer_url)
-
-
-midnight_in_paris = Movie(midnight_in_paris_title,
-                          midnight_in_paris_storyline,
-                          midnight_in_paris_poster,
-                          midnight_in_paris_trailer_url)
-
-hunger_games = Movie(hunger_games_title, hunger_games_storyline,
-                     hunger_games_poster, hunger_games_trailer_url)
+hunger_games = Movie('Hunger Games', 'A really real reality show',
+                     'https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg',
+                     'https://www.youtube.com/watch?v=LrXIG4oK7Ew')
 
 movies = [toy_story, avatar, school_of_rock, ratatouille,
           midnight_in_paris, hunger_games]
