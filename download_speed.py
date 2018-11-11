fbf917deb0af8c87c65a8cbbb032ec5862a09d00
Mahim <mahim216@gmail.com>
2018-11-11 05:50:52 -0500

    added example problem

---
+++ download_speed.py
@@ -0,0 +1,7 @@
+def download_time(gb):
+    megabytes_per_gigabytes = 1000
+    bits_per_byte = 8
+    megabits_per_second = 54
+    return gb * megabytes_per_gigabytes * bits_per_byte / megabits_per_second
+
+print(download_time(2))
