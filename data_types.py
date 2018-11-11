d39dfd7f606a950a142af275fe18282c9628feba
Mahim <mahim216@gmail.com>
2018-11-11 05:49:11 -0500

    removing code that doesnt work / unecessary code

--- data_types.py
+++
@@ -1,4 +0,0 @@
-from human import Human
-
-mahim = Human("Mahim Arib", 17, "male")
-print(str(mahim))
--- human.py
+++
@@ -1,9 +0,0 @@
-class Human:
-
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def __str__(self):
-        return self.name
